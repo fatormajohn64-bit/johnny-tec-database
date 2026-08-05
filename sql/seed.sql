2430de997cf762b916f052cf7ccc2c2fa7611377
Johnny-Tech <fatormajohn64@gmail.com>
2026-08-05 14:34:13 +0000

    Update seed.sql

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,18 +1,18 @@
--- Johnny Tec Developer Profile
-
-INSERT INTO developers
-(name, username, role)
-
+INSERT INTO developers (name, username, role, bio, github_url, primary_stack) 
 VALUES
-
 (
-'John Fatoma',
-'johnny-tec-dev',
-'Founder & Developer'
+    'John Fatoma',
+    'johnny-tec-dev',
+    'Founder & Lead Developer',
+    'Creator of Johnny Tec AI ecosystem.',
+    'https://github.com/johnny-tec-dev',
+    'Python, SQL, AI Architectures'
 ),
-
 (
-'Invisible 911',
-'invisible-911',
-'Developer Alias'
+    'Invisible 911',
+    'invisible-911',
+    'Developer Alias',
+    'Secondary developer profile and system alias.',
+    'https://github.com/invisible-911',
+    'Database Engineering & Security'
 );
