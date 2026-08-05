f1cfe538011b246223f6f21b311d977d6acdf479
Johnny-Tech <fatormajohn64@gmail.com>
2026-08-05 14:10:25 +0000

    Add developer profiles to seed.sql

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,1 +1,18 @@
+-- Johnny Tec Developer Profile
 
+INSERT INTO developers
+(name, username, role)
+
+VALUES
+
+(
+'John Fatoma',
+'johnny-tec-dev',
+'Founder & Developer'
+),
+
+(
+'Invisible 911',
+'invisible-911',
+'Developer Alias'
+);
